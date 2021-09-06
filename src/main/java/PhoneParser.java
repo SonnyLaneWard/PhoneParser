b45f11add574275bb8c95246f8d5c71a417ad03a
SonnyLaneWard <ivan.semejkin@yandex.ru>
2021-09-06 14:39:16 +0300

    ArrayListProdUI

--- src/main/java/PhoneParser.java
+++ src/main/java/PhoneParser.java
@@ -1,9 +1,20 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import java.util.ArrayList;
 import java.util.Arrays;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
-public class PhoneParser {
+
+public class PhoneParser extends  Application{
 
     public static void main(String[] args) {
+
+       Application.launch(args);
 
         ArrayList<String> arr = Reader.read(); //DATA
 
@@ -63,6 +74,42 @@ public class PhoneParser {
     }
 
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        // установка надписи
+        Text text = new Text("Data input");
+        text.setLayoutY(80);    // установка положения надписи по оси Y
+        text.setLayoutX(200);   // установка положения надписи по оси X
 
+        Button btn = new Button();
+        btn.setText("OK");
+        btn.setLayoutX(200);
+        btn.setLayoutY(450);
+
+        Label label = new Label();
+        label.setText("Input file name");
+        Label label2 = new Label();
+        label2.setText("Output file name");
+
+        TextField textField = new TextField();
+        TextField textField2 = new TextField();
+
+
+        GridPane gridpane = new GridPane();
+        gridpane.add(text,2 ,1);
+        gridpane.add(btn,2,2);
+        gridpane.add(label,2,3);
+        gridpane.add(textField,2,4);
+        gridpane.add(label2,2,5);
+        gridpane.add(textField2,2,6);
+
+
+        Scene scene = new Scene(gridpane);
+        stage.setScene(scene);
+        stage.setTitle("Phone Parser");
+        stage.setWidth(200);
+        stage.setHeight(200);
+        stage.show();
+    }
 }
 
